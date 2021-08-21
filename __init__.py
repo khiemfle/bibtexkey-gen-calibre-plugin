e9fb8accac9363644559ceb23a02d7e822f176e1
Khiem Tran <khiem111189@gmail.com>
2021-08-21 19:27:23 +0300

    Some initializing code

---
+++ __init__.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python2
+# vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+
+__license__   = 'GPL v3'
+__copyright__ = '2021, Khiem'
+__docformat__ = 'restructuredtext en'
+
+from calibre.gui2.actions import InterfaceAction
+from calibre.customize import InterfaceActionBase
+from calibre.gui2 import error_dialog
+from calibre.ebooks.metadata.meta import get_metadata, set_metadata
+
+class InterfacePluginBase(InterfaceActionBase):
+    name                = 'Add Google style Bibtex key'
+    description         = 'Auto add generated Google style BibTex key'
+    supported_platforms = ['windows', 'osx', 'linux']
+    author              = 'Khiem'
+    version             = (0, 0, 1)
+    minimum_calibre_version = (0, 7, 53)
+    actual_plugin = 'calibre_plugins.add_google_style_bibtex_key:AddGoogleStyleBibTexKey'
+
+class AddGoogleStyleBibTexKey(InterfaceAction):
+    name = 'Add Google style Bibtex key'
+    action_spec = (_('Generate Bibtex key'), None, None, None)
+    action_type = 'current'
+    
+    def genesis(self):
+        self.qaction.triggered.connect(self.gui.iactions['View']._view_calibre_books)
+        orig_func = self.gui.iactions['View']._view_calibre_books
+
+        def add_google_style_bibtex_key(book_ids):
+            # View the book(s)
+            orig_func(book_ids)
+            # Then update the date stamp
+            db = self.gui.library_view.model().db
+            bibtex_column = '#bibtexkey'
+            custom_columns = db.custom_field_keys()
+            # Make sure column exists
+            if bibtex_column not in custom_columns: 
+                return error_dialog(self.gui, 'Before running this plugin', 
+                        'You need to create a custom Bibtex key column called %s '%bibtex_column, show=True)
+            label = db.field_metadata.key_to_label(bibtex_column)
+            print(label)
+
+            for book_id in book_ids:
+                print(book_id)
+
+                current_bibtex = ''
+                try:
+                    current_bibtex = db.get_custom(book_id, label=label)
+                except:
+                    print('ignore')
+
+                print(current_bibtex)
+
+                if current_bibtex == '':
+                    mi = db.get_metadata(book_id, get_cover=False, cover_as_data=False)
+
+                    # db.author.last_name + db.published.year + db.first two words of the book title in lower case, for example: punamusta2020were
+
+                    print(mi.authors)
+
+                    first_author = db.get("authors").split(", ")[0].split(" ")
+                    first_author_name = first_author[0]
+                    if len(first_author) > 1:
+                        first_author_name = first_author_name[len(first_author)-1]
+
+                    print(db.get("pubdate"))
+                    published_year = db.get("pubdate").year
+
+                    print(db.get("title"))
+                    book_title_words = db.get("title").split(" ")
+
+                    book_title_first_words = book_title_words[0] + book_title_words[2]
+
+                    generated_bibtex_key = (first_author_name + published_year + book_title_first_words).lower()
+                    db.set_custom(book_id, generated_bibtex_key, label=label, commit=True)
+        # thanks to Kovid Goyal for the following line, also for Calibre in general
+        self.gui.iactions['View']._view_calibre_books = add_google_style_bibtex_key
---
+++ build.cmd
@@ -0,0 +1,5 @@
+REM https://sevenzip.osdn.jp/chm/cmdline/commands/add.htm
+del archived.zip
+
+7z a archived.zip __init__.py
+7z a archived.zip plugin-import-name-add_google_style_bibtex_key.txt
