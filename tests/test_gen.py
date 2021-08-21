e823a92e7a90e73287fe258855b211bf597c52b3
Khiem Tran <khiem111189@gmail.com>
2021-08-21 22:25:03 +0300

    Update structure

--- tests/test_gen.py
+++ tests/test_gen.py
@@ -1,6 +1,6 @@
-from ..gen import *
+from gen import gen
 import datetime
 
 # https://python.plainenglish.io/unit-testing-in-python-structure-57acd51da923
 def test_generate_bibtex():
-    generate_bibtex(["Kafka: The Definitive Guide"], datetime.datetime(2017, 7, 1), ["Neha Narkhede, Gwen Shapira & Todd Palino"])
+    gen.generate_bibtex(["Kafka: The Definitive Guide"], datetime.datetime(2017, 7, 1), ["Neha Narkhede, Gwen Shapira & Todd Palino"])
