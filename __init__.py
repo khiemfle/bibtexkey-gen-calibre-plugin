f77856cbbee157ace5955fcd9e5c7f78bbc95cf9
Khiem Tran <khiem111189@gmail.com>
2021-08-21 19:49:04 +0300

    Some improvement

--- __init__.py
+++ __init__.py
@@ -54,26 +54,41 @@ class AddGoogleStyleBibTexKey(InterfaceAction):
                 print(current_bibtex)
 
                 if current_bibtex == '':
-                    mi = db.get_metadata(book_id, get_cover=False, cover_as_data=False)
+                    # https://github.com/kovidgoyal/calibre/blob/3dd95981398777f3c958e733209f3583e783b98c/src/calibre/db/legacy.py
+                    mi = db.get_metadata(book_id, index_is_id=True, get_cover=False, cover_as_data=False)
 
                     # db.author.last_name + db.published.year + db.first two words of the book title in lower case, for example: punamusta2020were
 
                     print(mi.authors)
 
-                    first_author = db.get("authors").split(", ")[0].split(" ")
+                    print(mi.authors[0])
+
+                    print(mi.authors[0].split(", "))
+
+                    first_author = mi.authors[0].split(", ")[0].split(" ")
                     first_author_name = first_author[0]
                     if len(first_author) > 1:
-                        first_author_name = first_author_name[len(first_author)-1]
+                        first_author_name = first_author[len(first_author)-1]
 
-                    print(db.get("pubdate"))
-                    published_year = db.get("pubdate").year
+                    print(first_author_name)
 
-                    print(db.get("title"))
-                    book_title_words = db.get("title").split(" ")
+                    print(mi.pubdate)
+                    published_year = mi.pubdate.year
 
-                    book_title_first_words = book_title_words[0] + book_title_words[2]
+                    print(published_year)
 
-                    generated_bibtex_key = (first_author_name + published_year + book_title_first_words).lower()
+                    print(mi.title)
+                    book_title_words = mi.title.split(" ")
+
+                    print(book_title_words)
+
+                    book_title_first_words = book_title_words[0] + book_title_words[1]
+
+                    print(book_title_first_words)
+
+                    generated_bibtex_key = (first_author_name + str(published_year) + book_title_first_words).lower()
+
+                    print(generated_bibtex_key)
                     db.set_custom(book_id, generated_bibtex_key, label=label, commit=True)
         # thanks to Kovid Goyal for the following line, also for Calibre in general
         self.gui.iactions['View']._view_calibre_books = add_google_style_bibtex_key
--- build-and-debug.cmd
+++ build-and-debug.cmd
@@ -1,4 +1,4 @@
 REM call build.cmd
 
 REM https://stackoverflow.com/questions/8055371/how-do-i-run-two-commands-in-one-line-in-windows-cmd
-calibre-debug -g & calibre-customize -b "D:\workspace\Experiments\bibtexkey-gen-calibre-plugin" & calibre
+calibre-customize -b "D:\workspace\Experiments\bibtexkey-gen-calibre-plugin" & calibre-debug -g 
