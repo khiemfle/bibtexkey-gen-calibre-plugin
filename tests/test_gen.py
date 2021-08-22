51ba0657f0fd1f0a34482b18da8e99eb4104d0dd
Khiem Tran <khiem111189@gmail.com>
2021-08-22 13:08:13 +0300

    Improve tests

--- tests/test_gen.py
+++ tests/test_gen.py
@@ -3,25 +3,16 @@ import datetime
 
 # https://python.plainenglish.io/unit-testing-in-python-structure-57acd51da923
 def test_generate_bibtex1():
-    assert("narkhede2017kafka-ktdg" == gen.generate_bibtex(["Neha Narkhede, Gwen Shapira & Todd Palino"], datetime.datetime(2017, 7, 1), "Kafka: The Definitive Guide"))
+    cases = [
+        ["narkhede2017kafka-ktdg", ["Neha Narkhede, Gwen Shapira & Todd Palino"], datetime.datetime(2017, 7, 1), "Kafka: The Definitive Guide"],
+        ["kubernetes2021init-containers", ["Kubernetes"], datetime.datetime(2021, 7, 1), "Init Containers"],
+        ["aws2020aws-acliug", ["AWS"], datetime.datetime(2020, 7, 1), "AWS Command Line Interface - User Guide"],
+        ["author2019were-wrc",["An author"], datetime.datetime(2019, 7, 1), "We're really cool"],
+        ["author2019weetxt",["An author"], datetime.datetime(2019, 7, 1), "wee.txt"],
+        ["author2019were-wrc",["An author"], datetime.datetime(2019, 7, 1), "We`re really cool"],
+        ["author2019were-wrc",["An author"], datetime.datetime(2019, 7, 1), "We´re really cool"],
+        ["katz2019introduction-itmcpapchcanss",["Jonathan Katz & Yehuda Lindell"], datetime.datetime(2019, 7, 1), "Introduction to Modern Cryptography: Principles and Protocols (Chapman & Hall/CRC Cryptography and Network Security Series)"]
+    ]
 
-def test_generate_bibtex2():
-    assert("kubernetes2021init-containers" == gen.generate_bibtex(["Kubernetes"], datetime.datetime(2021, 7, 1), "Init Containers"))
-
-def test_generate_bibtex3():
-    assert("aws2020aws-acliug" == gen.generate_bibtex(["AWS"], datetime.datetime(2020, 7, 1), "AWS Command Line Interface - User Guide"))
-
-def test_generate_bibtex4():
-    assert("author2019were-wrc" == gen.generate_bibtex(["An author"], datetime.datetime(2019, 7, 1), "We're really cool"))
-
-def test_generate_bibtex5():
-    assert("author2019weetxt" == gen.generate_bibtex(["An author"], datetime.datetime(2019, 7, 1), "wee.txt"))
-    
-def test_generate_bibtex6():
-    assert("author2019were-wrc" == gen.generate_bibtex(["An author"], datetime.datetime(2019, 7, 1), "We`re really cool"))
-
-def test_generate_bibtex7():
-    assert("author2019were-wrc" == gen.generate_bibtex(["An author"], datetime.datetime(2019, 7, 1), "We´re really cool"))
-
-def test_generate_bibtex8():
-    assert("katz2019introduction-itmcpapchcanss" == gen.generate_bibtex(["Jonathan Katz & Yehuda Lindell"], datetime.datetime(2019, 7, 1), "Introduction to Modern Cryptography: Principles and Protocols (Chapman & Hall/CRC Cryptography and Network Security Series)"))
+    for case in cases:
+        assert(case[0] == gen.generate_bibtex(case[1], case[2], case[3]))
